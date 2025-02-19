ebf999e18947f3edeb68bb21e1eabaea6653bd7a
Tamir Malka <tamirmal@gmail.com>
2025-02-19 23:16:47 +0200

    working train

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from auraloss.time import SISDRLoss
 from LpLoss import LpLoss
 import torch.nn as nn
 import torch.optim as optim
+from tqdm import tqdm
+import random
 
 ###############################################################################
 # "the STFT is computed using a 32 ms Hann window with 50% overlap.""
@@ -78,19 +80,28 @@ def train_model(model, train_loader, val_loader, num_epochs=25, checkpoint_path=
     for epoch in range(start_epoch, num_epochs):
         model.train()
         running_loss = 0.0
-        for noisy, clean, noise in train_loader:
-            noisy, clean, noise = noisy.to(device), clean.to(device), noise.to(device)
-            optimizer.zero_grad()
-            noisy_stft = torch.stft(noisy, **stft_params)
-            noisy_mag = torch.abs(noisy_stft)
-            WF_stft, AMAP_stft, logvar = model(x=noisy_mag, noisy_complex=noisy_stft)
-
-            AMAP_istft = torch.istft(AMAP_stft, **stft_params)
-            clean_istft = torch.istft(clean, **stft_params)
-            loss = sisdr_loss_func(AMAP_istft, clean) + Lp_loss_func(WF_stft, logvar, clean_istft)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item() * noisy.size(0)
+        with tqdm(total=len(train_loader), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
+            for noisy, clean, noise in train_loader:
+                noisy, clean, noise = noisy.to(device), clean.to(device), noise.to(device)
+                # torch.STFT expects (B, T) but we have (B, 1, T). so squeeze the channel dimension
+                noisy_stft = torch.stft(noisy.squeeze(1), return_complex=True, **stft_params)
+                clean_stft = torch.stft(clean.squeeze(1), return_complex=True, **stft_params)
+                noisy_mag = torch.abs(noisy_stft)
+                optimizer.zero_grad()
+                # Network input expects: B, 1, T, F => F is last, while torch STFT returns T as last, lets permute
+                # and return it back to the original shape after the network for istft
+                x = noisy_mag.permute(0, 2, 1)
+                noisy_complex = noisy_stft.permute(0, 2, 1)
+                WF_stft, AMAP_stft, logvar = model(x=x, noisy_complex=noisy_complex)
+                AMAP_stft = AMAP_stft.permute(0, 2, 1)
+                WF_stft = WF_stft.permute(0, 2, 1)
+                logvar = logvar.permute(0, 2, 1)
+                AMAP_istft = torch.istft(AMAP_stft, **stft_params)
+                loss = sisdr_loss_func(AMAP_istft, clean) + Lp_loss_func(WF_stft, logvar, clean_stft)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item() * noisy.size(0)
+                pbar.update(1)
 
         epoch_loss = running_loss / len(train_loader.dataset)
         print(f'Epoch {epoch}/{num_epochs - 1}, Training Loss: {epoch_loss:.4f}')
@@ -104,9 +115,8 @@ def train_model(model, train_loader, val_loader, num_epochs=25, checkpoint_path=
         if val_loss < best_loss:
             best_loss = val_loss
             epochs_no_improve = 0
-            best_model_path = checkpoint_path
-            # Save the best model
-            save_checkpoint(model, optimizer, epoch + 1, val_loss, checkpoint_path)
+            best_model_path = f"best_model_epoch_{epoch}.pth"
+            save_checkpoint(model, optimizer, epoch + 1, val_loss, best_model_path)
         else:
             epochs_no_improve += 1
 
@@ -117,7 +127,12 @@ def train_model(model, train_loader, val_loader, num_epochs=25, checkpoint_path=
     return best_model_path
 
 if __name__ == "__main__":
-    dataset = SpeechDataset()
+    random.seed(7) # for consistency of dataset split
+    dataset = SpeechDataset(
+        noisy_dir="/Users/tamirmal/git/DNS_Challenge/datasets_generated/noisy",
+        clean_dir="/Users/tamirmal/git/DNS_Challenge/datasets_generated/clean",
+        noise_dir="/Users/tamirmal/git/DNS_Challenge/datasets_generated/noise"
+    )
     train_size = int(0.8 * len(dataset))
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
@@ -128,6 +143,6 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EDNet_uncertainty().to(device)
     best_model_path = train_model(model, train_loader, val_loader, num_epochs=25)
-    
+
     if best_model_path:
         print(f'Best model saved at: {best_model_path}')
